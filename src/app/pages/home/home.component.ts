aa30c64b1f0eb343840b555163e291efa519f6a3
Erick Arruzzo <earruzzo.rj@gmail.com>
2024-06-10 18:19:07 +0100

    Menu em home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,14 @@
 import { Component, effect, inject, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { PrecoFormularioComponent } from '../../shared/components/preco-formulario/preco-formulario.component';
-import { ScrollService } from '../../shared/services/scroll.service';
 import { RouterLink } from '@angular/router';
 import { StorageService } from '../../shared/services/storage.service';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatButtonModule, PrecoFormularioComponent, RouterLink],
+  imports: [MatButtonModule, PrecoFormularioComponent, RouterLink, MatButtonToggleModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
